bbcd0b46a0d8276c68f5cacbe18f6ca3b38c3022
Juliano Ventola <ventolajuliano@gmail.com>
2019-07-23 20:16:13 -0300

    Get and Post added to JS file using Jquery

--- public/app.js
+++ public/app.js
@@ -1,14 +1,38 @@
 $(document).ready(function () {
     $.getJSON("/api/todos")
-        .then(addTodos)
+        .then(loadTodos)
+
+    $("#todoInput").keypress(function (event) {
+        if (event.which == 13) {
+            createTodo();
+        }
+    });
 });
 
-function addTodos(todos) {
+function loadTodos(todos) {
     todos.forEach(element => {
-        var newTodo = $("<li class='task'>" + element.name + "</li>");
-        if (element.completed) {
-            newTodo.addClass("done");
-        }
-        $('.list').append(newTodo);
+        addTodo(element);
     });
 };
+
+function addTodo(todo) {
+    var newTodo = $("<li class='task'>" + todo.name + "</li>");
+    if (todo.completed) {
+        newTodo.addClass("done");
+    }
+    $('.list').append(newTodo);
+}
+
+function createTodo() {
+    var usrInput = $("#todoInput").val();
+    $.post("/api/todos", {
+            name: usrInput
+        })
+        .then(function (newTodo) {
+            $("#todoInput").val('');
+            addTodo(newTodo);
+        })
+        .catch(function (err) {
+            console.log(err);
+        })
+}
