f37d3a84850fcf9ad31c0eb7b715322b15ae2123
Juliano Ventola <ventolajuliano@gmail.com>
2019-07-23 21:16:40 -0300

    Update function Added to JS and App is Done!

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+
 $(document).ready(function () {
     $.getJSON("/api/todos")
         .then(loadTodos)
@@ -8,7 +9,12 @@ $(document).ready(function () {
         }
     });
 
-    $('.list').on('click', 'span', function() {
+    $('.list').on('click', 'li', function() {
+        updateTodo($(this));
+    });
+
+    $('.list').on('click', 'span', function(event) {
+        event.stopPropagation();
         removeTodo($(this).parent());
     });
 });
@@ -22,6 +28,7 @@ function loadTodos(todos) {
 function addTodo(todo) {
     var newTodo = $("<li class='task'>" + todo.name + "<span>X</span></li>");
     newTodo.data('id', todo._id);
+    newTodo.data('completed', todo.completed);
     if (todo.completed) {
         newTodo.addClass("done");
     }
@@ -40,6 +47,23 @@ function createTodo() {
         .catch(function (err) {
             console.log(err);
         })
+}
+
+function updateTodo(todo) {
+    var isDone = !todo.data('completed');
+    var updateData = {completed: isDone}
+    $.ajax({
+        method : 'PUT',
+        url :'/api/todos/' + todo.data('id'),
+        data: updateData
+    })
+    .then(function(updatedTodo) {
+        todo.toggleClass("done");
+        todo.data('completed', isDone);
+    })
+    .catch(function(err) {
+        console.log(err);        
+    })
 }
 
 function removeTodo(todo) {
